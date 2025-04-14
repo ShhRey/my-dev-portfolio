d585beb4ea56d5f593e9a1806299f7e345c24d16
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-14 13:58:16 -0400

    Messsing up CSS

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
                 {/* Splitting into Two Sides */}
                 <div className='showcase-layout'>
                     {/* Left Side: Solo Project */}
-                    <div className="first-project-wrapper proj-gradient-border">
+                    <div className="first-project-wrapper">
                         <div className="image-wrapper">
                             <img src="/images/TP-proj1.png" alt="Trading Platform" />
                         </div>
@@ -18,25 +18,19 @@ const Projects = () => {
                     </div>
 
                     {/* Right Side: Two Projects */}
-                    <div className='project-list-wrapper overflow-hidden proj-gradient-border'>
+                    <div className='project-list-wrapper overflow-hidden'>
                         <div className="project">
                             <div className="image-wrapper bg-[#8169C4]">
                                 <img src="/images/MLM-proj3.png" alt="Marketing Platform" />
                             </div>
-                            <div className="text-content">
-                                <h2>Smart referrals, Smarter networks</h2>
-                                <p className='text-white-50 md:text-xl'>A Django-React based Multi-Level Marketing platform featuring hierarchical dashboards, referral tracking, and admin/user portals.</p>
-                            </div>
+                            <h2>Smart referrals, Smarter networks</h2>
                         </div>
 
-                        <div className="project mt-10">
+                        <div className="project">
                             <div className="image-wrapper bg-[#1E364A]">
                                 <img src="/images/RN-proj2.png" alt="Movie App" />
                             </div>
-                            <div className="text-content">
-                                <h2>Explore movies at your fingertips</h2>
-                                <p className='text-white-50 md:text-xl'>A cross-platform mobile app built with React Native and TypeScript that fetches and displays movies from the TMDB API using debounced search and intuitive tab navigation.</p>
-                            </div>
+                            <h2>Explore movies at your fingertips</h2>
                         </div>
                     </div>
                 </div>
