163d0c9fb4ba03c033ee64078b3af4532ad0b30e
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-14 02:32:15 -0400

    Changed visibility for Counters

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -7,17 +7,22 @@ const AnimatedCounter = () => {
         <div id='counter' className='padding-x-lg xl:mt-0 mt-32'>
             <div className="mx-auto grid-4-cols">
                 {counterItems.map((item) => (
-                    <div className='flex flex-col justify-center bg-zinc-900 rounded-lg p-5'>
-                        <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
-                            <CountUp suffix={item.suffix} end={item.value} />
-                        </div>
-                        <div className='text-white-50 text-lg'>
-                            {item.label}
+                    <div
+                        key={item.label}
+                        className='flex justify-center items-center bg-zinc-900 rounded-lg p-5 h-32'
+                    >
+                        <div className='flex items-center gap-6'>
+                            <span className='text-white text-5xl font-bold flex items-center'>
+                                <CountUp end={item.value} />
+                                <span>{item.suffix}</span>
+                            </span>
+                            <span className='text-white-50 text-2xl font-semibold'>{item.label}</span>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
+
     )
 }
 
