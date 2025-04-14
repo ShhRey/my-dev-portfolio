824621363ceaba9dee39be78f94299df55c4ead6
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-14 01:07:39 -0400

    Handle background change for Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { navLinks } from '../constants'
 
 const NavBar = () => {
-  return (
-    <header className='navbar'>
-        <div className='inner'>
-            {/* DevName to return to Top */}
-            <a className='logo' href="#hero">Shrey Trivedi</a>
-            {/* Appear Only in Desktop Screen */}
-            <nav className='desktop'>
-                <ul>
-                    {navLinks.map(({ name, link }) => (
-                        <li className='group' key={name}>
-                            <a href={link}>
-                                <span style={{ textTransform: 'uppercase' }}>{name}</span>
-                                <span className='underline' />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            {/* Redirect to Contact Section */}
-            <a href="#contact" className='contact-btn group'>
-                <div className='inner'>
-                    <span>Contact Me</span>
-                </div>
-            </a>
-        </div>
-    </header>
-  )
+    // Create state to handle NavBar Background
+    const [scrolled, setScrolled] = useState(false);
+
+    // Handle changes according to Scroll
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 30;
+            setScrolled(isScrolled);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+    return (
+        <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
+            <div className='inner'>
+                {/* DevName to return to Top */}
+                <a className='logo' href="#hero">Shrey Trivedi</a>
+                {/* Appear Only in Desktop Screen */}
+                <nav className='desktop'>
+                    <ul>
+                        {navLinks.map(({ name, link }) => (
+                            <li className='group' key={name}>
+                                <a href={link}>
+                                    <span style={{ textTransform: 'uppercase' }}>{name}</span>
+                                    <span className='underline' />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                {/* Redirect to Contact Section */}
+                <a href="#contact" className='contact-btn group'>
+                    <div className='inner'>
+                        <span>Contact Me</span>
+                    </div>
+                </a>
+            </div>
+        </header>
+    )
 }
 
 export default NavBar
