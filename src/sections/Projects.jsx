e36c304cd60f858d4446d8e95912c56ed8b668d7
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-14 21:23:55 -0400

    Lazy load type animations for Projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
+// Importing GSAP Plugins for Animation
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
+    const sectionRef = useRef(null);
+    const proj1Ref = useRef(null);
+    const proj2Ref = useRef(null);
+    const proj3Ref = useRef(null);
+
+
+    // Runs when the component is Mounted
+    useGSAP(() => {
+        const projects = [proj1Ref.current, proj2Ref.current, proj3Ref.current];
+        projects.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                { y: 50, opacity: 0 },
+                { y: 0, opacity: 1, duration: 1, delay: 0.3 * (index + 1), scrollTrigger: { trigger: card, start: 'top bottom-=100' } },
+            )
+        })
+
+        gsap.fromTo(
+            sectionRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 1.5 }
+        )
+    }, []);
+
     return (
-        <div id="project" className='app-showcase'>
+        <section id="project" className='app-showcase' ref={sectionRef}>
             <div className="w-full">
                 {/* Splitting into Two Sides */}
                 <div className='showcase-layout'>
                     {/* Left Side: Solo Project */}
-                    <div className="first-project-wrapper">
+                    <div className="first-project-wrapper" ref={proj1Ref}>
                         <div className="image-wrapper">
                             <img src="/images/TP-proj1.png" alt="Trading Platform" />
                         </div>
@@ -19,14 +49,14 @@ const Projects = () => {
 
                     {/* Right Side: Two Projects */}
                     <div className='project-list-wrapper overflow-hidden'>
-                        <div className="project">
+                        <div className="project" ref={proj2Ref}>
                             <div className="image-wrapper bg-[#8169C4]">
                                 <img src="/images/MLM-proj3.png" alt="Marketing Platform" />
                             </div>
                             <h2>Smart referrals, Smarter networks</h2>
                         </div>
 
-                        <div className="project">
+                        <div className="project" ref={proj3Ref}>
                             <div className="image-wrapper bg-[#1E364A]">
                                 <img src="/images/RN-proj2.png" alt="Movie App" />
                             </div>
@@ -35,7 +65,7 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
