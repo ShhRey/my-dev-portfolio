e6dddd2bb1fe82e6b45bcf0298da6d946e6165d8
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-17 16:36:13 -0400

    Config changes for Vite+Github

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   // GitHub Repo name for base_url
-  base: '/my-dev-portfolio/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     react(),
     tailwindcss()
   ],
   resolve: {
     alias: {
-      '@images': path.resolve(path.dirname(), 'public/images'),
-      '@models': path.resolve(path.dirname(), 'public/models'),
+      '@images': path.resolve(__dirname, 'public/images'),
+      '@models': path.resolve(__dirname, 'public/models'),
     },
   },
 });
