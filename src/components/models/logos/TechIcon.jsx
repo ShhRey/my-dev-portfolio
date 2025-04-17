4b18f0fa57eb7b6916a9754d93326b07fa52f5b7
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-17 01:09:22 -0400

    Limiting zoom, rotations and revolutions for icons

--- src/components/models/logos/TechIcon.jsx
+++ src/components/models/logos/TechIcon.jsx
@@ -1,4 +1,4 @@
-import { Environment, Float, useGLTF } from '@react-three/drei'
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber';
 import React from 'react'
 
@@ -12,8 +12,19 @@ const TechIcon = ({ model }) => {
     <Canvas>
       {/* Add Lighting to view Model */}
       <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
       <Environment preset='city' />
 
+      {/* Setting OrbitControl to Rotate the Models */}
+      <OrbitControls
+        enableZoom={false}
+        minPolarAngle={Math.PI / 3}
+        maxPolarAngle={Math.PI / 1.5}
+        minAzimuthAngle={-Math.PI / 6}
+        maxAzimuthAngle={Math.PI / 6}
+      />
+
+      {/* Setting Float Speed and Rotation values from Constants */}
       <Float speed={5.5} rotationIntensity={1} floatIntensity={1}>
         <group scale={model.scale} rotation={model.rotation}>
           <primitive object={gltf.scene} />
