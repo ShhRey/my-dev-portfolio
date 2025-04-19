8210ce8ea8c02f9c3f34721943196106d4d8115d
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-19 00:58:58 -0400

    Trying to minify larger files and send data in chunks

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,22 @@ export default defineConfig(({ command }) => {
   return {
     // Add Base URL for GitHub Pages deployment if not in development
     base: isDev ? '/' : '/my-dev-portfolio/',
-    plugins: [
-      react(),
-      tailwindcss()
-    ],
+    plugins: [react(), tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // Splits big libraries like three.js, gsap, etc.
+            if (id.includes('node_modules')) {
+              if (id.includes('three')) return 'three'
+              if (id.includes('gsap')) return 'gsap'
+              if (id.includes('react')) return 'vendor'
+            }
+          },
+        },
+        // Set chunk size warning limit to 1MB
+        chunkSizeWarningLimit: 1000, 
+      },
+    },
   };
 });
