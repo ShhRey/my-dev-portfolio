e6fc22e350d6d2a049760e2b12b3ca545abc6f55
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-04-19 01:12:51 -0400

    Suggestion about base_url for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve';
-
-  return {
-    // Add Base URL for GitHub Pages deployment if not in development
-    base: isDev ? '/' : '/my-dev-portfolio/',
-    plugins: [ react(), tailwindcss() ],
-    build: {
-      // Increase the chunk size warning
-      chunkSizeWarningLimit: 1500,
-    },
-  };
+export default defineConfig({
+  // Vercel Deployment with Vite
+  base: '/',
+  plugins: [ react(), tailwindcss() ],
 });
